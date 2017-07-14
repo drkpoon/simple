2f12e73f29bb79e1dc083bdd7e45ba6ceed0f386
drkpoon <drkpoon@hgcbroadband.com>
2017-07-15 01:07:24 +0800

    change

--- index.js
+++ index.js
@@ -1,27 +1,8 @@
 var express = require('express');
 var app = express();
 
-var MongoClient = require('mongodb').MongoClient
-var collection;
-  
-console.log(process.env.OPENSHIFT_NODEJS_PORT);
-  
-var url = "mongodb://" + process.env.MONGODB_USER + ":" + process.env.MONGODB_PASSWORD
-	+ "@10.130.52.196:27017/anchors";
-  
-MongoClient.connect(url, function(err, db){
-	if (err)	{
-		console.log("fail to open");
-	}else{
-	console.log('connected');
-	collection = db.collection('profiles');
-		
-	}
-});
-
 app.get('/', function(req, res){
-	getNameList(function(namelist){
-		var currentdate = new Date(); 
+	var currentdate = new Date(); 
 	var datetime = "Last Sync: " + currentdate.getDate() + "/"
                 + (currentdate.getMonth()+1)  + "/" 
                 + currentdate.getFullYear() + " @ "  
@@ -29,28 +10,8 @@ app.get('/', function(req, res){
                 + currentdate.getMinutes() + ":" 
                 + currentdate.getSeconds();
 		
-    res.end(datetime + namelist);
-	});	
-	
+    res.end(datetime);
 });
 
 app.listen(8080);
 console.log('server is running...');
-
-function getNameList(callback){
-	if (collection == null){
-		callback(["no collection"]);
-	}
-	
-	collection.find().toArray(function(err, results){
-		if (err){
-			console.log('collection fails');
-			return;
-		}
-		namelist = [];
-		for (i=0; i<results.length; i++){
-			namelist.push(results[i].name);
-		}
-		callback(namelist);
-	});
-}
