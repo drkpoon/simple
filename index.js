8054fcd19fced5262567b39548cb832886c3adae
drkpoon <drkpoon@hgcbroadband.com>
2017-10-09 23:27:27 +0800

    change

--- index.js
+++ index.js
@@ -2,17 +2,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-	var currentdate = new Date(); 
-	var datetime = "Last Sync: " + currentdate.getDate() + "/"
-                + (currentdate.getMonth()+1)  + "/" 
-                + currentdate.getFullYear() + " @ "  
-                + currentdate.getHours() + ":"  
-                + currentdate.getMinutes() + ":" 
-                + currentdate.getSeconds();
-	var output = 'Current Time: ' + Date();
-	output = output + '\n' + currentdate.getTimezoneOffset();
-	output = output + '\n' + currentdate.getTime();
-    res.end(output);
+	var currentdate = new Date();
+    
+	res.render(__dirname + '/views/onload.ejs', {epoch: currentdate.getTime(), serverTime: currentdate});
 });
 
 var port = 8080;
